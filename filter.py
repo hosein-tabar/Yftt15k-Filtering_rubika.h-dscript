07204932b5397c8c35e9082dc8a1cbc68a1b8905
hosein-tabar <99099261+hosein-tabar@users.noreply.github.com>
2022-02-06 12:50:46 +0330

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,9 @@
+# Yftt15k-Filtering_rubika.h-dscript
+import sex
+import rubika
+import sex
+..........
+print ("http://pronhun.com https://s4.uupload.ir/files/screenshot_۲۰۲۱۱۱۰۲-۱۱۵۶۳۶_01e7.png https://s6.uupload.ir/files/img_20220201_011248_668_e3ij_thumb.jpg https://uupload.ir/view/img_20220201_011248_668_e3ij.jpg https://xexx.com")
+sex.https://uupload.ir/view/img_20220201_011248_668_e3ij.jpg
+https://uupload.ir/view/img_20220201_011248_668_e3ij.jpg
+https://uupload.ir/view/img_20220201_011248_668_e3ij.jpg
